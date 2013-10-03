ea1608964ba734898a8f14bcd52aa06985d3a6cd
Chris Odlaug <codlaug@wetpaint-inc.com>
2013-10-03 11:29:24 -0700

    Mst3k quotes

---
+++ plugins/mst3k/mst3k.rb
@@ -0,0 +1,28 @@
+require 'nokogiri'
+require 'open-uri'
+
+class Mst3k < Isis::Plugin::Base
+
+  def respond_to_msg?(msg, speaker)
+    msg.downcase == '!mst3k'
+  end
+
+  PAGES = %w{
+    http://www.svamcentral.org/mst/quotes/a_c.htm
+    http://www.svamcentral.org/mst/quotes/d_f.htm
+    http://www.svamcentral.org/mst/quotes/g_h.htm
+    http://www.svamcentral.org/mst/quotes/i_l.htm
+    http://www.svamcentral.org/mst/quotes/m_p.htm
+    http://www.svamcentral.org/mst/quotes/q_t.htm
+    http://www.svamcentral.org/mst/quotes/u_w.htm
+    http://www.svamcentral.org/mst/quotes/x_z.htm
+  }
+
+  private
+
+  def response_text
+    page = Nokogiri.HTML(open(PAGES.sample))
+    quote = page.css('p')[1..-1].sample.text
+    "#{quote}"
+  end
+end
